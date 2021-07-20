d7283898a8d29a1c3df194ead238285ae82edbd1
fumi2mi <imafumi.murataka@gmail.com>
2021-07-20 19:38:51 +0900

    想定通りに動くようになりました

--- imamooKadai6/GameViewController.swift
+++ imamooKadai6/GameViewController.swift
@@ -10,29 +10,44 @@ import UIKit
 class GameViewController: UIViewController {
     @IBOutlet private weak var randomNumberLabel: UILabel!
     @IBOutlet private weak var guessSlider: UISlider!
-    
+
+    private var randomNumber = 50
+    private var guessNumber = 50
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setRandomNumberLabel()
+        guessSlider.value = (guessSlider.maximumValue + guessSlider.minimumValue) / 2
     }
 
     @IBAction private func judgementButtonPressed(_ sender: UIButton) {
         let isMatch = judgeMatch()
-        print(isMatch)
+        aleartResult(isMatch)
     }
     
     private func setRandomNumberLabel() {
-        let randomNuber = Int.random(in: 1...100)
-        randomNumberLabel.text = String(randomNuber)
+        randomNumber = Int.random(in: 1...100)
+        randomNumberLabel.text = String(randomNumber)
     }
     
     private func judgeMatch() -> Bool {
         guard let randomNumber = Int(randomNumberLabel.text ?? "") else {
             return false
         }
-        let guessNumber = Int(guessSlider.value)
+        guessNumber = Int(guessSlider.value)
         return randomNumber == guessNumber
+    }
+    
+    private func aleartResult(_ isMatch: Bool) {
+        let title = "結果"
+        let message = (isMatch ? "あたり!" : "はずれ!") + "\nあなたの値: \(guessNumber)"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(
+            UIAlertAction(title: "再挑戦", style: .default, handler: {action in
+                self.setRandomNumberLabel()
+            })
+        )
+        present(alert, animated: true, completion: nil)
     }
 }
 
